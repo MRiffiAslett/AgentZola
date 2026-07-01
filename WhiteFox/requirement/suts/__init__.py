6357ac5af8168f88a4707f7212f5348b3099de6d
Max Riffi-Aslett <max.riffi3@gmail.com>
2026-07-01 20:30:12 +0100

    Remove unused TFLite and Inductor requirement generators

--- WhiteFox/requirement/suts/__init__.py
+++ WhiteFox/requirement/suts/__init__.py
@@ -1,10 +1,6 @@
 from requirement.suts.xla import XLARequirementGenerator
-from requirement.suts.inductor import InductorRequirementGenerator
-from requirement.suts.tflite import TFLiteRequirementGenerator
 
 __all__ = [
     "XLARequirementGenerator",
-    "InductorRequirementGenerator",
-    "TFLiteRequirementGenerator",
 ]
 
